4b1d420e0a0159e0f68990323fb0380a2d8baba8
beissssen <154684787+beissssen@users.noreply.github.com>
2024-05-15 23:44:07 +0500

    Create MyTestingClass.java

---
+++ MyTestingClass.java
@@ -0,0 +1,22 @@
+import java.util.Objects;
+import java.util.Random;
+
+
+public class MyTestingClass {
+    private int id;
+
+    public MyTestingClass(int id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public int hashCode() {
+
+        int hash = 7;
+        hash = 31 * hash + id;
+        return hash;
+    }
+
+
+}
